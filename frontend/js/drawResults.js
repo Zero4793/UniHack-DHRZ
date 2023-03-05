f3b2a83531a1563a17e4e1c1e0f3faa06e3fe5b9
rgrzazek <rgrzazek@myune.edu.au>
2023-03-05 11:23:34 +1000

    Prototype of results display

---
+++ frontend/js/drawResults.js
@@ -0,0 +1,40 @@
+let canvas;
+$(window).on("load", async function() {
+  canvas = $("#results").get(0);
+  canvas.width = 640;
+  canvas.height = 640;
+  g = canvas.getContext('2d');
+  console.log("loaded")
+  draw5(Array(68, 42, 75, 33, 100));
+  console.log("drawn");
+});
+
+async function draw5(attributes) {
+  g.clearRect(0, 0, canvas.width, canvas.height);
+  g.fillStyle = '#BECFEA';
+  g.fillRect(0, 0, 640, 640);
+
+  // width of one bar
+  let width = (canvas.width-20)/5;
+
+  for (let i = 0; i < 5;i++) {
+    let thisX = width*i;
+    let height =  ((canvas.height-10) * (attributes[i]/100));
+    
+    let red = 50*i;
+    let green = (101*i)%255
+    let blue = 255 - (50*i)
+    g.fillStyle = "#ffffff";
+    g.fillRect(15+i*width, canvas.height - height, width-10, height-10);
+      
+  }
+
+  summaryText = 
+  "Openness ("+attributes[0]+"%): Abstract, intellectual rather than concrete and traditional.<br>\
+  Conscientiousness ("+attributes[1]+"%): Disciplined rather than impulsive.<br>\
+  Extraversion ("+attributes[2]+"%): Seek stimulation from outside and attention rather than conserving energy.<br>\
+  Agreeableness ("+attributes[3]+"%): Put others needs first and cooperate rather than win/lose.<br>\
+  Neuroticism ("+attributes[4]+"%): Tendency to experience negative emotions, react rather than move on.<br>"
+
+  $("#summary").html(summaryText);
+}
